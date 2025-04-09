b4d144cd2d619906b70cd92675bde2f4f397c046
Ray Dev <142141276+RayDev07@users.noreply.github.com>
2025-04-09 21:27:19 +0530

    Update dm.js

--- src/commands/Moderation/dm.js
+++ src/commands/Moderation/dm.js
@@ -8,7 +8,7 @@ module.exports = {
     args: false,
     usage: "",
     userPerms: [],
-    owner: false,
+    owner: true,
     premium: false,
     execute: async (message, args, client, prefix) => {
  
